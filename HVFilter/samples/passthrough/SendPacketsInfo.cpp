5f54062bcd21b367973eca9e619907eddb690538
Zenith <fio_244@yahoo.com>
2013-06-22 14:53:36 +0300

    HVFilter: based on sample on net

---
+++ HVFilter/samples/passthrough/SendPacketsInfo.cpp
@@ -0,0 +1,11 @@
+#include "SendPacketsInfo.h"
+
+void push_buffers_info_lists_inbound(PNET_BUFFER_LIST NetBufferLists, ULONG NumberOfNetBufferLists)
+{
+
+}
+
+void push_buffers_info_lists_outbound(PNET_BUFFER_LIST NetBufferLists, ULONG NumberOfNetBufferLists)
+{
+
+}
---
+++ HVFilter/samples/passthrough/SendPacketsInfo.h
@@ -0,0 +1,4 @@
+#include <ndis.h>
+
+void push_buffers_info_lists_send(PNET_BUFFER_LIST NetBufferLists, ULONG NumberOfNetBufferLists);
+void push_buffers_info_lists_receive(PNET_BUFFER_LIST NetBufferLists, ULONG NumberOfNetBufferLists);
