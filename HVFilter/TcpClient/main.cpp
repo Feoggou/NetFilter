657cbd09a639a6e5b734184599c2d8dc2f2a3476
Zenith <fio_244@yahoo.com>
2013-08-12 00:42:26 +0300

    rename TestTcpIp -> TcpClient. Also modify tcpclient: it sends and receives from server.

--- HVFilter/TcpClient/main.cpp
+++ HVFilter/TcpClient/main.cpp
@@ -3,17 +3,22 @@
 
 int main(int argc, const char* argv[])
 {
+	std::string sIP;
+	int port = 0;
+
 	if (argc < 3) {
-		std::cout << "call: testtcpip 192.168.0.2 9000" << std::endl;
-		return -1;
+		sIP = "173.194.32.46";
+		port = 80;
+	} else {
+		sIP = argv[1];
+		port = atoi(argv[2]);
+		if (port < 1) {
+			std::cout << "port number too small!" << std::endl;
+			return -1;
+		} 
 	}
 
-	std::string sIP = argv[1];
-	int port = atoi(argv[2]);
-	if (port < 1) {
-		std::cout << "port number too small!" << std::endl;
-		return -1;
-	}
+	std::cout << "calling startup..." << std::endl;
 
 	WSADATA wsaData;
 	int result = WSAStartup(MAKEWORD(2,2), &wsaData);
@@ -28,6 +33,8 @@ int main(int argc, const char* argv[])
 
 	/**********************/
 
+	std::cout << "creating socket..." << std::endl;
+
 	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (s == INVALID_SOCKET)
 	{
@@ -37,21 +44,13 @@ int main(int argc, const char* argv[])
 
 	std::cout << "created socket" << std::endl;
 
-	int time = 5000;
-	int nError = setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (const char*)&time, sizeof(time));
-	if (nError != 0)
-	{
-		std::cout << "socket error: " << WSAGetLastError() << std::endl;
-		return WSAGetLastError();
-	}
-
-	std::cout << "set socket blocking" << std::endl;
-
 	//173.194.32.46 80
 	SOCKADDR_IN server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.S_un.S_addr = inet_addr(sIP.data());
 	server_address.sin_port = htons(port);
+
+	std::cout << "connecting to " << sIP.data() << " port" << port << std::endl;
 
 	//client
 	if (0 != connect(s, (SOCKADDR*)&server_address, sizeof(server_address)))
@@ -62,13 +61,27 @@ int main(int argc, const char* argv[])
 
 	std::cout << "connected to: ip=" <<  sIP.data() << "; port=" << port << std::endl;
 
-	result = send(s, "mata este proasta!", 19, 0);
+	std::cout << "sending message..." << std::endl;
+
+	result = send(s, "whatever", 9, 0);
 	if (SOCKET_ERROR == result) {
 		std::cout << "socket error: " << WSAGetLastError() << std::endl;
 		return WSAGetLastError();
 	}
 
-	std::cout << "sent message" << std::endl;
+	std::cout << "message sent. receiving..." << std::endl;
+
+	char buf[9];
+	result = recv(s, buf, 9, 0);
+	if (SOCKET_ERROR == result) {
+		std::cout << "socket error: " << WSAGetLastError() << std::endl;
+		return WSAGetLastError();
+	}
+
+	std::cout << "finished receiving. sleeping..." << std::endl;
+
+	Sleep(1000);
+	std::cout << "awaken. shutting down..." << std::endl;
 
 	result = shutdown(s, SD_BOTH);
 	if (result) {
@@ -76,7 +89,7 @@ int main(int argc, const char* argv[])
 		return WSAGetLastError();
 	}
 
-	std::cout << "shut down" << std::endl;
+	std::cout << "shut down finished. cleaning up..." << std::endl;
 
 	if (WSACleanup() != 0)
 	{
@@ -84,7 +97,7 @@ int main(int argc, const char* argv[])
 		return WSAGetLastError();
 	}
 
-	std::cout << "cleanup!" << std::endl;
+	std::cout << "cleanup finished!" << std::endl;
 
 	return 0;
 }
